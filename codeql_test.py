a11a91427f36b3cab1077e3b239d6ca2659f66d8
Bakul Gupta <bakulgupta11@gmail.com>
2024-05-16 12:01:21 +0530

    Update codeql_test.py

--- codeql_test.py
+++ codeql_test.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 import pickle
 
 
-def unsafer(pickled):
+def unsaferr(pickled):
     return pickle.loads(pickled)
 
 
 urlpatterns = [
-    url(r'^(?P<object>.*)$', unsafer)
+    url(r'^(?P<object>.*)$', unsaferr)
 ]
 
 urlpatternss = [
-    url(r'^(?P<object>.*)$', unsafer)
+    url(r'^(?P<object>.*)$', unsaferr)
 ]
